4a5867728e8ae8e881a6b2283d69ecd3f27eaf18
jkcookiejk <cookjkcomputer@gmail.com>
2023-01-31 09:04:31 +0900

    한수

--- src/main/java/com/fd/common/model/vo/PageInfo.java
+++ src/main/java/com/fd/common/model/vo/PageInfo.java
@@ -2,4 +2,90 @@ package com.fd.common.model.vo;
 
 public class PageInfo {
 
+	private int listCount; // 현재 게시글 총 갯수 
+	private int currentPage; // 사용자가 요청한 페이지 (== 현재페이지)
+	private int pageLimit; // 페이지 하단에 보여질 페이징바의 페이지 최대갯수 (몇개단위씩)
+	private int boardLimit; // 한 페이지 내에 보여질 게시글 최대갯수(몇개 단위씩)
+	private int maxPage; // 가장 마지막 페이지 (총 페이지수)
+	private int startPage; // 사용자가 요청한 페이지 하단의 페이징바의 시작수 
+	private int endPage; // 사용자가 요청한 페이지 하단의 페이징바의 끝수 
+	
+	public PageInfo() {}
+
+	public PageInfo(int listCount, int currentPage, int pageLimit, int boardLimit, int maxPage, int startPage,
+			int endPage) {
+		super();
+		this.listCount = listCount;
+		this.currentPage = currentPage;
+		this.pageLimit = pageLimit;
+		this.boardLimit = boardLimit;
+		this.maxPage = maxPage;
+		this.startPage = startPage;
+		this.endPage = endPage;
+	}
+
+	public int getListCount() {
+		return listCount;
+	}
+
+	public void setListCount(int listCount) {
+		this.listCount = listCount;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getPageLimit() {
+		return pageLimit;
+	}
+
+	public void setPageLimit(int pageLimit) {
+		this.pageLimit = pageLimit;
+	}
+
+	public int getBoardLimit() {
+		return boardLimit;
+	}
+
+	public void setBoardLimit(int boardLimit) {
+		this.boardLimit = boardLimit;
+	}
+
+	public int getMaxPage() {
+		return maxPage;
+	}
+
+	public void setMaxPage(int maxPage) {
+		this.maxPage = maxPage;
+	}
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+	public int getEndPage() {
+		return endPage;
+	}
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+
+	@Override
+	public String toString() {
+		return "PageInfo [listCount=" + listCount + ", currentPage=" + currentPage + ", pageLimit=" + pageLimit
+				+ ", boardLimit=" + boardLimit + ", maxPage=" + maxPage + ", startPage=" + startPage + ", endPage="
+				+ endPage + "]";
+	}
+	
+	
 }
--- src/main/java/com/fd/restaurant/model/vo/Restaurant.java
+++ src/main/java/com/fd/restaurant/model/vo/Restaurant.java
@@ -87,7 +87,33 @@ public class Restaurant {
 		this.breakE = breakE;
 		this.foodCt = foodCt;
 	}
+	
+	
+	// 한수가 만든 매개변수생성자 (SearchDao클래스의 selectList()에서 사용!!)
+	public Restaurant(String resName, String address, String foodCt) {
+		super();
+		this.resName = resName;
+		this.address = address;
+		this.foodCt = foodCt;
+	}
+	
+	// 한수가 만든 매개변수생성자 (ResFormController의 doGet()에서 사용!!)
+	public Restaurant(String resName, String ceo, String permitNo, String address, String dAddress, String phone,
+			String cellphone, String email, String parking, String foodCt) {
+		super();
+		this.resName = resName;
+		this.ceo = ceo;
+		this.permitNo = permitNo;
+		this.address = address;
+		this.dAddress = dAddress;
+		this.phone = phone;
+		this.cellphone = cellphone;
+		this.email = email;
+		this.parking = parking;
+		this.foodCt = foodCt;
+	}
 
+	
 	public int getResNo() {
 		return resNo;
 	}
