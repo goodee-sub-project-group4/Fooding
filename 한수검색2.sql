e3bf432469b797e363c9ca8744e1dcda116d3c0d
jkcookiejk <cookjkcomputer@gmail.com>
2023-02-07 00:06:34 +0900

    한수검색2.sql 업로드
    한수검색2.sql 파일 업로드!!

---
+++ 한수검색2.sql
@@ -0,0 +1,126 @@
+--- 한수 
+
+-- 서울특별시 금천구 western 13개 
+-- 1) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '세라즈피자펍', '변백현', '149-25-14542', '서울시 금천구 가산동', '371-28 A동 2F 222호', '서울특별시', '금천구', '02-852-0415', '010-1234-1234', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020115211325536.jpg', 'western', '25');  
+
+-- 2) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '아웃백스테이크하우스', '설현', '189-22-14772', '서울시 금천구 가산동', '60-8 7F', '서울특별시', '금천구', '02-2136-9971', '010-1234-1234', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020115212852088.jpg', 'western', '7');  
+
+-- 3)
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '샐러드로', '이영애', '741-22-18872', '서울시 금천구 독산동', '291-1 현대지식산업센터 T-L124호', '서울특별시', '금천구', '02-2181-3580', '010-1234-1234', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020116334721983.jpg', 'western', '13');  
+
+-- 4)
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '매드포갈릭', '김철수', '195-28-17242', '서울시 금천구 가산동', '60-8 현대시티아울렛 8F', '서울특별시', '금천구', '02-2136-9982', '010-1234-1234', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020116392430027.jpg', 'western', '42');  
+
+-- 5) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '라티시크릿셰프', '박라희', '144-55-19642', '서울시 금천구 독산동', '1156 롯데캐슬골드파크타워 B1', '서울특별시', '금천구', '02-2136-9982', '02-6487-4545', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'western', '55');  
+
+-- 6) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '더제이케이키친박스', '안제희', '177-33-19112', '서울시 금천구 가산동', '371-50 에이스하이앤드타워3차', '서울특별시', '금천구', '02-2136-9982', '010-8524-4111', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020119093674987.jpg', 'western', '95');
+
+-- 7) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '오픈더그린', '서유리', '124-88-16941', '서울시 금천구 가산동', '345-29', '서울특별시', '금천구', '02-2135-9209', '010-7412-9652', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'western', '142');
+
+-- 8) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '화덕피자모넬로', '시우민', '142-66-19222', '서울시 금천구 독산동', '1155 롯데캐슬골드파크 3차상가 LF 08호', '서울특별시', '금천구', '010-6465-1373', '02-7466-7496', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020212241651753.jpg', 'western', '96');
+
+-- 9) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '빅터스버거', '김제니', '141-77-77112', '서울시 금천구 독산동', '1155 롯데캐슬골드파크3차 126호', '서울특별시', '금천구', '02-803-5668', '010-1475-9856', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'western', '74');
+
+-- 10) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '셀렉티', '라리사', '162-13-54693', '서울시 금천구 가산동', '152-30', '서울특별시', '금천구', '02-303-7735', '010-1111-2222', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020212252814329.jpg', 'western', '86');
+
+-- 11) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '롱블랙스푼', '김민지', '112-42-34569', '서울시 금천구 가산동', '371-17 BYC하이시티', '서울특별시', '금천구', '02-6947-7788', '010-1245-4545', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020212223094755.jpg', 'western', '36');
+
+-- 12)
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '마피아피자펍', '성해은', '155-32-19112', '서울시 금천구 가산동', '371-28 우림라이온스밸리 A동 B1 B106-1호', '서울특별시', '금천구', '02-2136-9982', '010-4258-4545', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020418285044082.jpg', 'western', '76');
+
+-- 13)
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '리샐러드', '정규민', '127-47-19241', '서울시 금천구 독산동', '1150 113호', '서울특별시', '금천구', '02-5853-4322', '010-1428-4545', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020521011265683.jpg', 'western', '93');
+
+
+-- 경기도 금천구 japanese 
+-- 1) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '스시히또', '김남운', '174-37-16641', '서울시 금천구 가산동', '60-15 B1 114호', '서울특별시', '금천구', '02-6953-0105', '010-1427-4335', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020210123053635.jpg', 'japanese', '44');
+
+-- 일단 여기까지는 사진 다 다르게 넣음.. 
+
+--2) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '단또', '정호산', '155-30-44735', '서울시 금천구 가산동', '60-5 갑을그레이트밸리 2F', '서울특별시', '금천구', '02-857-1852', '010-2274-1436', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'japanese', '66');
+
+--3)
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '스시옥', '김태령', '188-13-16441', '서울시 금천구 독산동', '291-6 1F', '서울특별시', '금천구', '02-806-7877', '010-1427-4335', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'japanese', '80');
+
+
+-- 경기도 파주시 western 
+--1) 
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '혜밀버거레시피', '박지인', '174-14-92456', '경기도 파주시 탄현면', '법흥리 1652-190', '경기도', '파주시', '031-949-1652', '010-7415-4542', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'western', '69');
+
+--2)
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '갈릴리농원', '전정욱', '114-11-92446', '경기도 파주시 탄현면', '낙하리 4-1', '경기도', '파주시', '031-942-8400', '010-1425-4542', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'western', '14');
+
+--3)
+INSERT INTO RESTAURANT (RES_NO, USER_PWD, RES_NAME, CEO, PERMIT_NO, ADDRESS, D_ADDRESS, LOCAL_CT, DLOCAL_CT, PHONE, CELLPHONE, EMAIL, APPLY_DATE, LONGTITUDE, LATITUDE, R_IMG, FOOD_CT, COUNT)
+VALUES (SEQ_RESNO.NEXTVAL, '1234', '더티트렁크', '김석인', '114-65-92786', '경기도 파주시 탄현면', '문발동 75-6 1-2F', '경기도', '파주시', '031-946-9283', '010-1456-7463', 'mimi@naver.com', SYSDATE, 
+'10', '20', 'resources/rest_upfiles/2023020120361514930.jpg', 'western', '38');
+
+COMMIT;
+
+
+
+
+
+
+
+
+
+
+-- 서울특별시 금천구 japanese 7개 
+
+-- 경기도 파주시 13개 
+
+-- 제주도 서귀포시 13개  
+
+-----------------------------------------------------------------------------------------
+
+
+
+
