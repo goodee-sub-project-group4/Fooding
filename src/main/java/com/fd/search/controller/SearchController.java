8946cee708ba128169fa0e135c5055f055d15ab5
jkcookiejk <cookjkcomputer@gmail.com>
2023-08-03 00:35:22 +0900

    .

--- src/main/java/com/fd/search/controller/SearchController.java
+++ src/main/java/com/fd/search/controller/SearchController.java
@@ -32,55 +32,36 @@ public class SearchController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		request.setCharacterEncoding("UTF-8");
-		
 		// 1) 사용자로부터 전달받은 값 뽑아서 변수에 저장 
 		String localCt = request.getParameter("city");	
 		String dLocalCt = request.getParameter("county"); 
 		String foodCt = request.getParameter("foodCt"); 
-		
-		
 		// 2) 페이징처리를 위한 변수 설정 
-		int listCount; 		// 현재 게시글 총 갯수 ( => 검색결과 총 갯수로 쓰자!! )
-		int currentPage; 	// 사용자가 요청한 페이지 ( == 현재 페이지)
-		int pageLimit; 		// 페이지 하단에 보여질 페이징바의 페이지 최대갯수 (몇개 단위씩)
-		int boardLimit; 	// 한 페이지 내에 보여질 게시글 최대 갯수 (몇개 단위씩)
+		int listCount; 		
+		int currentPage; 	
+		int pageLimit; 		
+		int boardLimit; 	
+		int maxPage; 		
+		int startPage; 		
+		int endPage; 		
 		
-		int maxPage; 		// 가장 마지막페이지 (총 페이지수)
-		int startPage; 		// 서용자가 요청한 페이지 하단의 페이징바의 시작수
-		int endPage; 		// 사용자가 요청한 페이지 하단의 페이징바의 끝수 
-		
-		// 3) listCount (총 게시글 갯수 => 검색결과 총 갯수)
 		listCount = new SearchService().selectListCount(localCt, dLocalCt, foodCt);
-		
-		// 4) currentPage : 사용자가 요청한 페이지 (현재 페이지)
 		currentPage = Integer.parseInt(request.getParameter("cpage")); 
-		
-		// 5) pageLimit : 페이징바의 페이지 최대 갯수 
 		pageLimit = 10; 
-		
-		// 6) boardLimit : 한 페이지당 보여질 검색게시글 최대 갯수 
 		boardLimit = 10; 
-		
-		// 7) maxPage : 제일 마지막 페이지수(총 페이지수)
 		maxPage = (int)Math.ceil( (double)listCount / boardLimit ); 
-		
-		// 8) startPage : 페이징바의 시작수 
 		startPage = (currentPage - 1) / pageLimit * pageLimit + 1; 
-		
-		// 9 ) endPage : 페이징바의 끝수 
 		endPage = startPage + pageLimit - 1; 
 		
-		// 10) 
 		if(endPage > maxPage) {
 			endPage = maxPage; 
 		}
 		
-		// 11) 페이징바를 만들때 필요한 객체 생성
+		// 3) 페이징바를 만들때 필요한 객체 생성
 		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
 		
-		// 12) 현재 요청한 페이지(currentPage)에 보여질 검색게시글 리스트 boardLimit 수만큼 조회 	
+		// 4) 현재 요청한 페이지(currentPage)에 보여질 검색게시글 리스트 boardLimit 수만큼 조회 	
 		ArrayList<Restaurant> list = new SearchService().selectList(pi, localCt, dLocalCt, foodCt); 
 		
 		request.setAttribute("pi", pi); 
